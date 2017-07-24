de2f1a0609461611679ba0bb57737d9903759bad
Anton Savin <anton.savin@gmail.com>
2017-07-24 19:53:23 +1000

    checkDiet impl

--- module1/app.js
+++ module1/app.js
@@ -4,7 +4,18 @@
       $scope.messageOnUrDiet = "";
 
       $scope.checkDiet = function() {
-        $scope.messageOnUrDiet = "test";
+
+
+        var itemArray = $scope.items.split(',');
+        if (itemArray.length == 1 && itemArray[0].length == 0) {
+          $scope.messageOnUrDiet = "Please enter data first";
+        }
+        else if (itemArray.length < 4) {
+          $scope.messageOnUrDiet = "Enjoy!";
+        }
+        else {
+          $scope.messageOnUrDiet = "Too much!";
+        }
       };
 
     });
